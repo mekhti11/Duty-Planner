034a3e6e3b5344343172f3a7ddf3a8138d1ce75c
Mekhti <mekhti11@gmail.com>
2025-06-21 05:02:52 +0300

    feat: setting up domain and core layer

--- domain/src/main/java/com/example/dutyplanner/domain/usecase/DeleteDutyUseCase.kt
+++ domain/src/main/java/com/example/dutyplanner/domain/usecase/DeleteDutyUseCase.kt
@@ -1,9 +1,13 @@
 package com.example.dutyplanner.domain.usecase
 
 import com.example.dutyplanner.domain.repository.DutyRepository
+import javax.inject.Inject
 
-class DeleteDutyUseCase(private val repo: DutyRepository) {
-    suspend operator fun invoke(id: Int) {
-        repo.deleteDuty(id)
+class DeleteDutyUseCase @Inject constructor(
+    private val repository: DutyRepository
+) {
+    suspend operator fun invoke(dutyId: String): Result<Unit> {
+        require(dutyId.isNotBlank()) { "Duty ID cannot be blank" }
+        return repository.deleteDuty(dutyId)
     }
 }
